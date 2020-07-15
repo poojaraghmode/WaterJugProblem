ddd97fcbd0880c8a536fafec16a1bb949c3d6749
Pooja Raghmode <44026238+poojaraghmode@users.noreply.github.com>
2020-07-15 12:45:25 +0530

    Update WaterJugProblem.cpp

--- WaterJugProblem.cpp
+++ WaterJugProblem.cpp
@@ -3,29 +3,39 @@
 #include <stdio.h>
 #include<math.h>
 #include <conio.h>
+
 int min(int a, int b);
+
 using namespace std;
+
 void pour(int fromCapacity, int toCapacity, int d)
 {
 	int temp;
 	int from = fromCapacity, to = 0, step = 1;
+	
 	cout << "\nStep\tJug M\tJug N\n";
 	cout << step << "\t" << from << "\t" << to << "\n";
+	
 	while (from != d && to != d)
 	{
 		temp = min(from, toCapacity - to);
+		
 		to += temp;
 		from -= temp;
 		step++;
+		
 		cout << step << "\t" << from << "\t" << to << "\n";
+		
 		if (from == d || to == d)
 			break;
+		
 		if (to == toCapacity)
 		{
 			to = 0;
 			step++;
 			cout << step << "\t" << from << "\t" << to << "\n";
 		}
+		
 		if (from == 0)
 		{
 			from = fromCapacity;
@@ -35,6 +45,7 @@ void pour(int fromCapacity, int toCapacity, int d)
 		}
 	}
 }
+
 int min(int a, int b)
 {
 	if (a < b)
@@ -42,6 +53,7 @@ int min(int a, int b)
 	else
 		return b;
 }
+
 int gcd(int a, int b)
 {
 	while (a != b)
@@ -53,14 +65,19 @@ int gcd(int a, int b)
 	}
 	return a;
 }
+
 int main()
 {
 	int m, n, d;
+	
 	cout << "WATER JUG PROBLEM\n";
+	
 	cout << "Enter value of m:\n";
 	cin >> m;
+	
 	cout << "Enter value of n:\n";
 	cin >> n;
+	
 	cout << "Enter value of d:\n";
 	cin >> d;
 
